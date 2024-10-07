85f993d6a99c8f6123f38b453c4d29fbe12551cc
abunaserkayes0 <abunaserkayes0@gmail.com>
2024-10-07 23:52:24 +0600

    feat:Mongodb with SocialProvider

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,36 +1,40 @@
+import { auth } from "@/libs/auth";
 import Image from "next/image";
 import Link from "next/link";
-// import logo from "/logo.svg";
-const Navbar = () => {
+const Navbar = async ({ mode }) => {
+  const session = await auth();
+  console.log(session);
+
   return (
     <nav>
       <Link href="/">
         <Image src="/logo.svg" width={150} height={150} alt="Logo" />
       </Link>
+      {mode && (
+        <ul>
+          <li>
+            <Link href="#">Recommended Places</Link>
+          </li>
 
-      <ul>
-        <li>
-          <Link href="#">Recommended Places</Link>
-        </li>
+          <li>
+            <Link href="#">About Us</Link>
+          </li>
 
-        <li>
-          <Link href="#">About Us</Link>
-        </li>
+          <li>
+            <Link href="#">Contact us</Link>
+          </li>
 
-        <li>
-          <Link href="#">Contact us</Link>
-        </li>
+          <li>
+            <Link href="/bookings">Bookings</Link>
+          </li>
 
-        <li>
-          <Link href="/bookings">Bookings</Link>
-        </li>
-
-        <li>
-          <Link href="/login" className="login">
-            Login
-          </Link>
-        </li>
-      </ul>
+          <li>
+            <Link href="/login" className="login">
+              Login
+            </Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
--- components/auth/SocialLogins.jsx
+++ components/auth/SocialLogins.jsx
@@ -1,15 +1,61 @@
+"use client";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter, usePathname } from "next/navigation";
 
-const SocialLogins = () => {
+const SocialLogins = ({ mode }) => {
+  /**
+   * Local State
+   */
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  /**
+   * Hooks
+   */
+  const router = useRouter();
+  const pathname = usePathname();
+
+  /**
+   * Handlers
+   */
+  const handelAuth = (providerName, providerCallbackUrl) => {
+    signIn(providerName, { callbackUrl: providerCallbackUrl });
+  };
+
   return (
     <>
-      <div className="text-center text-xs text-gray-500">or SignUp with</div>
+      <div className="text-center text-xs text-gray-500">
+        or{" "}
+        {mode === "login" ? (
+          <Link className="underline" href="/signup">
+            SignUp
+          </Link>
+        ) : (
+          <Link className="underline" href="/login">
+            Login
+          </Link>
+        )}{" "}
+        with
+      </div>
       <div className="flex gap-4">
-        <button className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center">
+        <button
+          onClick={() =>
+            handelAuth("facebook", "https://localhost:3000/hotels")
+          }
+          className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center"
+        >
           <Image src="/fb.png" alt="facebook" width={40} height={40} />
           <span>Facebook</span>
         </button>
-        <button className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center">
+        <button
+          onClick={() =>
+            handelAuth("google", "https://localhost:3000/bookings")
+          }
+          className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center"
+        >
           <Image src="/google.png" alt="google" width={40} height={40} />
           <span>Google</span>
         </button>
