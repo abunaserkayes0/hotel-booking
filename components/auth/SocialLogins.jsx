87d0b59a0855f85005a84bc42cc436dcad83c9e6
abunaserkayes0 <abunaserkayes0@gmail.com>
2024-10-20 20:42:53 +0600

    Fixed:Error fixed problem

--- components/auth/SocialLogins.jsx
+++ components/auth/SocialLogins.jsx
@@ -42,18 +42,14 @@ const SocialLogins = ({ mode }) => {
       </div>
       <div className="flex gap-4">
         <button
-          onClick={() =>
-            handelAuth("facebook", "https://localhost:3000/hotels")
-          }
+          onClick={() => handelAuth("facebook", "/hotels")}
           className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center"
         >
           <Image src="/fb.png" alt="facebook" width={40} height={40} />
           <span>Facebook</span>
         </button>
         <button
-          onClick={() =>
-            handelAuth("google", "https://localhost:3000/bookings")
-          }
+          onClick={() => handelAuth("google", "/bookings")}
           className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center"
         >
           <Image src="/google.png" alt="google" width={40} height={40} />
